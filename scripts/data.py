838788952282af314da742168b58e0935a3b7cf8
KumaTea <KumaTea@outlook.com>
2023-06-03 16:25:10 +0800

    download external media / build

--- scripts/data.py
+++ scripts/data.py
@@ -1,20 +1,31 @@
-posts = {
-    '220616': {
-        'name': 'we-in-game'
-    },
-    '220623': {
-        'name': 'nexmo'
-    },
-    '220710': {
-        'name': 'pi-rhel'
-    },
-    '221003': {
-        'name': 'glibc-openwrt'
-    },
-    '221227': {
-        'name': 'pve-openwrt'
-    },
-    '230603': {
-        'name': 'bank-cards'
-    },
-}
+import os
+import requests
+
+
+# posts = {
+#     '220616': {
+#         'name': 'we-in-game'
+#     },
+# }
+
+pwd = os.getcwd()
+posts_path = os.path.join(pwd, 'posts')
+
+all_posts = {}
+for i in os.listdir(posts_path):
+    # 220531-hello-world
+    date = i.split('-')[0]
+    title = i[len(date) + 1:]
+    all_posts[date] = {
+        'name': title
+    }
+
+tags_api = 'https://api.github.com/repos/KumaTea/blog/git/refs/tags'
+r = requests.get(tags_api)
+tags_json = r.json()
+tags = [i['ref'].split('/')[-1] for i in tags_json]
+
+posts = {}
+for i in tags:
+    if i in all_posts.keys():
+        posts[i] = all_posts[i]
--- scripts/media.py
+++ scripts/media.py
@@ -1,7 +1,11 @@
 import os
+import re
+import random
+import string
 import logging
 import requests
 import subprocess
+import urllib.request
 from data import posts
 
 
@@ -14,27 +18,85 @@ media_file = 'media.zip'
 
 
 def get_media(post_id):
-    media_path = os.path.join(temp_path, post_id, media_file)
-    os.makedirs(os.path.dirname(media_path), exist_ok=True)
-    with open(media_path, 'wb') as f:
+    temp_media_path = os.path.join(temp_path, post_id, media_file)
+    os.makedirs(os.path.dirname(temp_media_path), exist_ok=True)
+    with open(temp_media_path, 'wb') as f:
         r = requests.get(f'https://github.com/{repo}/releases/download/{post_id}/{media_file}')
         f.write(r.content)
     return logging.info(f'post {post_id} downloaded')
 
 
 def unzip_media(post_id):
-    media_path = os.path.join(temp_path, post_id, media_file)
-    destination = os.path.join(
-        pwd,
-        'posts',
-        '{post_id}-{title}'.format(
-            post_id=post_id,
-            title=posts[post_id]['name']
-        )
-    )
-    os.makedirs(destination, exist_ok=True)
-    subprocess.run(['unzip', media_path, '-d', destination])
+    temp_media_path = os.path.join(temp_path, post_id, media_file)
+    post_name = '{post_id}-{title}'.format(post_id=post_id, title=posts[post_id]['name'])
+    post_path = os.path.join(pwd, 'posts', post_name)
+    os.makedirs(post_path, exist_ok=True)
+    subprocess.run(['unzip', temp_media_path, '-d', post_path])
     return logging.info(f'post {post_id} unzipped')
+
+
+def gen_uuid(length=4):
+    # https://stackoverflow.com/a/56398787/10714490
+    alphabet = string.ascii_lowercase + string.digits
+    return ''.join(random.choices(alphabet, k=length))
+
+
+def get_content_type(url):
+    with urllib.request.urlopen(url) as response:
+        return response.headers['content-type']
+
+
+def get_external_media(post_id):
+    used_uuid = []
+    post_name = '{post_id}-{title}'.format(post_id=post_id, title=posts[post_id]['name'])
+    post_file = os.path.join(pwd, 'posts', post_name, 'index.md')
+    media_path = os.path.join(pwd, 'posts', post_name, 'img')
+    os.makedirs(os.path.dirname(media_path), exist_ok=True)
+
+    # media in markdown: ![description](link)
+    media_regex = r'!\[.*\]\((.*)\)'
+    media_md = []
+    with open(post_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    for line in lines:
+        if re.search(media_regex, line):
+            media_md.append(re.search(media_regex, line).group(1))
+
+    # check if media is external
+    url_regex = r'(https?):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])'
+    edited = False
+    for media in media_md:
+        if re.search(url_regex, media):
+            edited = True
+            # download media
+            media_name = gen_uuid()
+            while media_name in used_uuid:
+                media_name = gen_uuid()
+            used_uuid.append(media_name)
+
+            media_url = media
+            media_type = get_content_type(media_url)
+            media_ext = media_type.split('/')[1]
+            if media_ext == 'jpeg':
+                media_ext = 'jpg'
+
+            media_filename = f'{media_name}.{media_ext}'
+            with open(os.path.join(media_path, media_filename), 'wb') as f:
+                r = requests.get(media_url)
+                f.write(r.content)
+
+            # replace media url
+            for i, line in enumerate(lines):
+                if media in line:
+                    lines[i] = line.replace(media, f'img/{media_filename}')
+            logging.info(f'external {media} downloaded')
+
+    if edited:
+        with open(post_file, 'w', encoding='utf-8') as f:
+            f.writelines(lines)
+        return logging.info(f'post {post_id} media replaced')
+    else:
+        return None
 
 
 if __name__ == '__main__':
@@ -43,6 +105,7 @@ if __name__ == '__main__':
     for pid in posts:
         get_media(pid)
         unzip_media(pid)
+        get_external_media(pid)
 
     subprocess.run(['rm', '-rvf', temp_path])
     logging.info('done')
